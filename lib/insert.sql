3ab5c76dad2b39a8589be8e6df1ef4fdbc3c7887
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-11 19:10:47 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "male", "brown", "happy", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "male", "red", "wild", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabith", 4, "female", "white", "lazy", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "male", "brown", "happy", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "female", "black", "sad", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 13, "female", "brown", "stingy", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "male", "green", "agressive", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, "female", "gray", "angry", 1)
